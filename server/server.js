aaf13641ff590fc845a73a1d165b763264a5baba
dev-seack <dev@ts-dm.de>
2018-11-13 14:19:09 +0100

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -54,16 +54,27 @@ io.on("connection", (socket) => {
   });
 
   socket.on("createMessage", (message, callback) => {
-    console.log("new Message", message);
-    io.emit("newMessage", generateMessage(message.from, message.text)); // emits a event to ervery connection
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit(
+        "newMessage",
+        generateMessage(user.name, message.text)
+      ); // emits a event to ervery connection
+    }
+
     callback();
   });
 
   socket.on("createLocationMessage", (coords) => {
-    io.emit(
-      "newLocationMessage",
-      generateLocationMessage("Admin", coords.latitude, coords.longitude)
-    );
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "newLocationMessage",
+        generateLocationMessage(user.name, coords.latitude, coords.longitude)
+      );
+    }
   });
 
   socket.on("disconnect", () => {
